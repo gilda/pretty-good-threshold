4a3979ad0c0b3a2b2551136183d9040a52d5947b
gilda <gil@daganmail.com>
2019-12-14 12:59:47 +0200

    change aes to aes-gcm for GMAC with string functions

--- src/aes/aes.cpp
+++ src/aes/aes.cpp
@@ -2,55 +2,137 @@
 
 // TODO make sure key is 256 bit and iv is 128 bit
 namespace AES{
-	int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext){
-		
+	int gcm_encrypt(unsigned char *plaintext, int plaintext_len,
+                	unsigned char *aad, int aad_len,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len,
+                	unsigned char *ciphertext,
+                	unsigned char *tag){
 		EVP_CIPHER_CTX *ctx;
 		int len;
 		int ciphertext_len;
-		
-		// create and initialise the context
-		if(!(ctx = EVP_CIPHER_CTX_new())) {}
-		
-		// initialise the encryption operation
-		if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) {}
 
-		// provide the message to be encrypted, and obtain the encrypted output
-		if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) {}
+		/* Create and initialise the context */
+		if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+		/* Initialise the encryption operation. */
+		if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) handleErrors();
+
+		/*
+		* Set IV length if default 12 bytes (96 bits) is not appropriate
+		*/
+		if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv_len, NULL)) handleErrors();
+
+		/* Initialise key and IV */
+		if(1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+		/*
+		* Provide any AAD data. This can be called zero or more times as
+		* required
+		*/
+		if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len)) handleErrors();
+
+		/*
+		* Provide the message to be encrypted, and obtain the encrypted output.
+		* EVP_EncryptUpdate can be called multiple times if necessary
+		*/
+		if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) handleErrors();
 		ciphertext_len = len;
 
-		// finalise the encryption
-		if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) {}
+		/*
+		* Finalise the encryption. Normally ciphertext bytes may be written at
+		* this stage, but this does not occur in GCM mode
+		*/
+		if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
 		ciphertext_len += len;
 
-		// clean up
+		/* Get the tag */
+		if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag)) handleErrors();
+
+		/* Clean up */
 		EVP_CIPHER_CTX_free(ctx);
 
 		return ciphertext_len;
 	}
 
+	int gcm_encrypt(std::string plaintext,
+                	std::string aad,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len,
+                	unsigned char *ciphertext,
+                	unsigned char *tag){
+		return gcm_encrypt((unsigned char *)plaintext.c_str(), plaintext.length(), (unsigned char *)aad.c_str(), aad.length(), key, iv, iv_len, ciphertext, tag);
+	}
 
-	int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext){
+
+	int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
+                	unsigned char *aad, int aad_len,
+                	unsigned char *tag,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len,
+                	unsigned char *plaintext){
 		EVP_CIPHER_CTX *ctx;
 		int len;
 		int plaintext_len;
+		int ret;
 
-		// create and initialise the context
-		if(!(ctx = EVP_CIPHER_CTX_new())) {}
+		/* Create and initialise the context */
+		if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-		// initialise the decryption operation
-		if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) {}
+		/* Initialise the decryption operation. */
+		if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) handleErrors();
 
-		// provide the message to be decrypted, and obtain the plaintext output
-		if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) {}
+		/* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+		if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv_len, NULL)) handleErrors();
+
+		/* Initialise key and IV */
+		if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+		/*
+		* Provide any AAD data. This can be called zero or more times as
+		* required
+		*/
+		if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len)) handleErrors();
+
+		/*
+		* Provide the message to be decrypted, and obtain the plaintext output.
+		* EVP_DecryptUpdate can be called multiple times if necessary
+		*/
+		if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) handleErrors();
 		plaintext_len = len;
 
-		// finalise the decryption
-		if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) {}
-		plaintext_len += len;
+		/* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+		if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) handleErrors();
 
-		// clean up
+		/*
+		* Finalise the decryption. A positive return value indicates success,
+		* anything else is a failure - the plaintext is not trustworthy.
+		*/
+		ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+		/* Clean up */
 		EVP_CIPHER_CTX_free(ctx);
 
-		return plaintext_len;
+		if(ret > 0) {
+			/* Success */
+			plaintext_len += len;
+			return plaintext_len;
+		} else {
+			/* Verify failed */
+			return -1;
+		}
+	}
+
+	std::string gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
+                	std::string aad,
+                	unsigned char *tag,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len){
+		// create a buffer big enough
+		std::string ret;
+		ret.reserve(ciphertext_len);
+		
+		gcm_decrypt(ciphertext, ciphertext_len, (unsigned char *)aad.c_str(), aad.length(), tag, key, iv, iv_len, (unsigned char *)&ret[0]);
+		return ret;
 	}
 }
--- src/aes/aes.h
+++ src/aes/aes.h
@@ -1,8 +1,33 @@
 #pragma once
+#include <string>
 #include <openssl/evp.h>
 #include "../util/util.h"
 
 namespace AES{
-	int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext);
-	int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext);
+	int gcm_encrypt(unsigned char *plaintext, int plaintext_len,
+                	unsigned char *aad, int aad_len,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len,
+                	unsigned char *ciphertext,
+                	unsigned char *tag);
+
+	int gcm_encrypt(std::string plaintext,
+                	std::string aad,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len,
+                	unsigned char *ciphertext,
+                	unsigned char *tag);
+
+	int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
+                	unsigned char *aad, int aad_len,
+                	unsigned char *tag,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len,
+                	unsigned char *plaintext);
+
+	std::string gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
+                	std::string aad,
+                	unsigned char *tag,
+                	unsigned char *key,
+                	unsigned char *iv, int iv_len);
 };
